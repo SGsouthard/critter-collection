7fdf918d37c102e759789514321473f482c3c8a2
SGSouthard <sydneyaileensouthard@gmail.com>
2021-12-13 18:01:30 -0800

    AUGH

--- seeders/20211214012904-demo-bugs.js
+++ seeders/20211214012904-demo-bugs.js
@@ -2,17 +2,6 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Bugs', [
-      {
-        name: 'tiger-butterfly',
-        price: 240,
-        flickPrice: 360,
-        catchPhrase: 'I caught a tiger butterfly! I've earned my stripes!',
-        museumInfo: "Tiger butterflies are known for their majestic wings, which many consider quite beautiful. Truth be told, I find them monstrous! Those strange strip…"
-        image: "https://acnhapi.com/v1/images/bugs/3",
-        icon: "https://acnhapi.com/v1/icons/bugs/3",
-      }
-    ])
     /**
      * Add seed commands here.
      *
